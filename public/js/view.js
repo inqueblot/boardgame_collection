6ab0a214bca914840ff6dbc305c459a7ea9da5dc
Chad Clark <chadclark12@hotmail.com>
2021-02-13 12:11:44 -0600

    frontend js with api

---
+++ public/js/view.js
@@ -0,0 +1,45 @@
+"strict";
+$(document).ready(function () {
+  // SEARCH GAME NAME INPUT AND SUBMIT BUTTON \\
+  $("#search").on("click", function (event) {
+    event.preventDefault();
+    gameName = $("#game-name-input").val().trim();
+    getGameId(gameName);
+  });
+
+  // FIRST 3RD PARTY API CALL TO RETRIEVE ID \\
+  const getGameId = (gameName) => {
+    // ADD URL BELOW \\
+    const retrieveIdURL = `url ${gameName} here`;
+    $.ajax9({
+      url: retrieveIdURL,
+      method: "GET",
+    }).then(function (response) {
+      const id = response.id;
+      // ADD URL BELOW \\
+      const retrieveGameInfo = `url ${id} here`;
+      // SECOND 3RD PARTY API CALL WITH ID TO RETRIEVE GAME INFO \\
+      $.ajax({
+        url: retrieveGameInfo,
+        method: "GET",
+      }).then(function (response) {
+        // CREATE OUR RESPONSE OBJECT \\
+        const newGame = {
+          numberOfPlayers: 6,
+          gameCategory: fun,
+        };
+        sendNewGameDb(newGame);
+      });
+    });
+  };
+
+  const sendNewGameDb = (newGame) => {
+    $.ajax("/api/game", {
+      method: "POST",
+      data: newGame,
+    }).then(function () {
+      console.log("created new game");
+      location.reload();
+    });
+  };
+});
