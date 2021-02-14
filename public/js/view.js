6eff72dedbc98e3281ae8002f07a390f6de95394
inqueblot <notminebydesign@gmail.com>
2021-02-14 11:52:53 -0600

    updated html to point to correct folders

---
+++ public/js/view.js
@@ -0,0 +1,8 @@
+
+
+$("#search").on("submit", function (event) {
+    event.preventDefault();
+    console.log("button pressed")
+    // let search = $("#game-name-input").val();
+    // console.log(search)
+})
