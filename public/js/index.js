6e4bd5f01716471c2d8af2f4afb8c5c87990a6f0
inqueblot <notminebydesign@gmail.com>
2021-02-14 23:59:20 -0600

    search appends a small image and title

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function () {
   // SEARCH GAME NAME INPUT AND SUBMIT BUTTON \\
-  $("#search").on("submit", function (event) {
+  $("#search").on("click", function (event) {
     event.preventDefault();
     gameName = $("#game-name-input").val().trim();
     getGameId(gameName);
@@ -12,14 +12,19 @@ $(document).ready(function () {
   // FIRST 3RD PARTY API CALL TO RETRIEVE ID \\
   const getGameId = (gameName) => {
     // ADD URL BELOW \\
-    const gameSearch = `https://api.boardgameatlas.com/api/search?fuzzy_match=true&name=${gameName}&limit=2&order_by=popularity&pretty=true&client_id=JLBr5npPhV`;
+    const gameSearch = `https://api.boardgameatlas.com/api/search?fuzzy_match=true&name=${gameName}&limit=5&pretty=true&client_id=JLBr5npPhV`;
     $.ajax({
       url: gameSearch,
       method: "GET",
     }).then(function (response) {
       response.games.forEach(element => {
-        console.log(element)
-
+        console.log(element);
+        let html = `<p><img src=${element.images.small}>`;
+        html += `<br>${element.name}`;
+        html += `</p>`;
+        $("#searchResults").append(html);
+        //create event listener for image
+        //maybe add button to add to collection
       });
 
     });
