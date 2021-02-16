c2ad6a383620879bf66d6251f937c00173a0196b
inqueblot <notminebydesign@gmail.com>
2021-02-16 00:15:49 -0600

    clean up js, get image to populate

--- public/js/game.js
+++ public/js/game.js
@@ -8,4 +8,7 @@ $.ajax({
     method: "GET"
 }).then(function (response) {
     console.log(response)
+    let game = response.games[0];
+    console.log(game)
+    $("#game-search-result").append(`<img src=${game.image_url}>`)
 })
