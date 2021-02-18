f9fc45fe31d9a24cc46fe2ffc2500f72acc89d61
inqueblot <notminebydesign@gmail.com>
2021-02-18 01:38:21 -0600

    user can't add game to collection  twice

--- public/js/collection.js
+++ public/js/collection.js
@@ -1,5 +1,9 @@
 
 
+// var slider = document.getElementById("player-slider")
+// var output = document.getElementById('player-output');
+// output.innerHTML = slider.value;
+// slider.oninput = function () { output.innerHTML = this.value; }
 // // window.onload = collection()
 
 
@@ -19,4 +23,23 @@ $.get("/api/collection", function (data) {
     });
 });
 
+$("#numPlay").on("click", function (event) {
+    event.preventDefault();
+    $.get("/api/collection/players/:number", function (data) {
+        $("#table_results").children().remove()
+        data.forEach(element => {
+            let table = `<tr><td><a href="/game/${element.bg_id}"</a>${element.name}</td>`
+            table += `<td>${element.minPlayers}</td>`
+            table += `<td>${element.maxPlayers}</td>`
+            table += `<td>${element.playTime}</td>`
+            table += `<td>${element.yearPub}</td>`
+            table += `<td>${element.publisher}</td>`
+            table += `<td>${element.age}</td>`
+            table += `<td>${element.msrp}</td>`
+            table += `<td>${element.designer}</td>`
+            table += `<td><div class="buttons"><a class="button is-primary">delete</a></div></td></tr>`
+            $("#table_results").append(table)
+        });
+    });
+})
 
