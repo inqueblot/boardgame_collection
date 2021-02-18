c5678b1ee9756c9c551e11eef954a24aed7057e4
inqueblot <notminebydesign@gmail.com>
2021-02-18 10:40:10 -0600

    added playtime and designer

--- public/js/collection.js
+++ public/js/collection.js
@@ -1,12 +1,4 @@
 
-
-// var slider = document.getElementById("player-slider")
-// var output = document.getElementById('player-output');
-// output.innerHTML = slider.value;
-// slider.oninput = function () { output.innerHTML = this.value; }
-// // window.onload = collection()
-
-
 $.get("/api/collection", function (data) {
     data.forEach(element => {
         let table = `<tr><td><a href="/game/${element.bg_id}"</a>${element.name}</td>`
@@ -20,6 +12,8 @@ $.get("/api/collection", function (data) {
         table += `<td>${element.designer}</td>`
         table += `<td><div class="buttons"><a class="button is-primary">delete</a></div></td></tr>`
         $("#table_results").append(table)
+        let option = `<option>${element.designer}</option>`
+        $("#designerList").append(option)
     });
 });
 
@@ -50,3 +44,30 @@ let playerCount = function () {
     })
 }
 playerCount()
+
+// let playerCount = function () {
+//     $("#numPlay").on("keypress", function (event) {
+//         // event.preventDefault();
+//         if (event.which == 13) {
+//             // console.log("hello world")
+//             $.get(`/api/collection/players/${this.value}`, function (data) {
+//                 $("#table_results").children().remove()
+//                 data.forEach(element => {
+//                     let table = `<tr><td><a href="/game/${element.bg_id}"</a>${element.name}</td>`
+//                     table += `<td>${element.minPlayers}</td>`
+//                     table += `<td>${element.maxPlayers}</td>`
+//                     table += `<td>${element.playTime}</td>`
+//                     table += `<td>${element.yearPub}</td>`
+//                     table += `<td>${element.publisher}</td>`
+//                     table += `<td>${element.age}</td>`
+//                     table += `<td>${element.msrp}</td>`
+//                     table += `<td>${element.designer}</td>`
+//                     table += `<td><div class="buttons"><a class="button is-primary">delete</a></div></td></tr>`
+//                     $("#table_results").append(table)
+//                 }).then(function (results) {
+//                     playerCount();
+//                 });
+//             });
+//         }
+//     })
+// }
